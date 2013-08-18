30e218f54672423087ae16843e958415130ea9b1
Nicholas J. Kisseberth <nkissebe@purdue.edu>
2013-08-18 17:38:09 -0400

    [BC][COM_PROJECTS] self installer fixes for J25; fixes from testing

--- components/com_projects/helpers/helper.php
+++ components/com_projects/helpers/helper.php
@@ -835,13 +835,13 @@ class ProjectsHelper extends JObject {
 	public function virusCheck( $fpath = '' ) 
 	{
 		exec("clamscan -i --no-summary --block-encrypted " . escapeshellarg($fpath), $output, $status);
-		
+
 		if ($status == 1)
 		{
 			unlink($fpath);
 			return true;
 		}
-		
+
 		return false;
 	}
 	
